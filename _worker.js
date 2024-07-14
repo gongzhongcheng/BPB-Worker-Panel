eb1e461bc52c75031b9d8a6033f4104477260d20
gongzhongcheng <58355625+gongzhongcheng@users.noreply.github.com>
2024-07-14 13:59:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '2e80c2f1-1e99-d6c7-c4b8-04c1a4026cc4';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
